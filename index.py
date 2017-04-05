9a6cae02031a775f3a0fb7d1c15906af182b3b78
charles ryu ryu <charles ryu ryu>
2017-04-05 01:21:07 -0700

    mydictionary

---
+++ index.py
@@ -0,0 +1,12 @@
+def dictionary ():
+    myinfo = {
+        'Name': 'Charles', 
+        'Age': 22, 'Height': 5, 
+        'BrithCountry': 'North Korea',
+        'Favorite Programming Language': 'Python'
+        }
+    print "My name is " + myinfo['Name']+"."
+    print "I am " + str(myinfo['Age']) + " Years Old."
+    print "I am from " + myinfo['BrithCountry']+"."
+    print "My Favorite Programming Language is " + myinfo['Favorite Programming Language']
+dictionary()
